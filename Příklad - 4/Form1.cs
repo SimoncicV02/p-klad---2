63762c474f09f8ccb1d13b47b22dbafe6fe3a027
simoncicv.02@spst.eu <simoncicv.02@spst.eu>
2020-11-24 09:42:02 +0100

    Přidejte soubory projektu.

---
+++ Příklad - 4/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Příklad___4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cislo = int.Parse(comboBox1.SelectedItem.ToString());
+            int cislo1 = int.Parse(textBox1.Text);
+            int cislo2 = int.Parse(textBox2.Text);
+            int cislo3 = int.Parse(textBox3.Text);
+            int mv = cislo1;
+            for (int i = cislo; i > 1; i--)
+            {
+                cislo1 *= mv;
+            }
+            mv = cislo2;
+            for (int i = cislo; i > 1; i--)
+            {
+                cislo2 *= mv;
+            }
+            mv = cislo3;
+            for (int i = cislo; i > 1; i--)
+            {
+                cislo3 *= mv;
+            }
+            label1.Text = "Číslo 1: " + cislo1 +Environment.NewLine + "Číslo 2: " + cislo2 +Environment.NewLine + "Číslo 3: " + cislo3;
+        }
+    }
+}
---
+++ příklad - 2/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace příklad___2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();            
+            Random nh = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int x = nh.Next(0, 11);
+                listBox1.Items.Add(x);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int soucet = 0;
+            foreach(int a in listBox1.SelectedItems)
+            {
+                soucet += a;
+            }
+            label1.Text = "Počet vybraných položek: " + listBox1.SelectedItems.Count;
+            label2.Text = "Součet vybraných položek: " + soucet;
+        }
+    }
+}
