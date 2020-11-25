f0df04432f808cf3c660f37c06e58e4b11b7262f
Vojtěch Tesař <vtesar@centrum.cz>
2020-11-25 16:07:11 +0100

    Přídání příkladů 1 a 3

--- příklad - 1/Form1.cs
+++ příklad - 1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace příklad___1
@@ -14,11 +7,44 @@ namespace příklad___1
     {
         public Form1()
         {
-
-
-
-
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string pismena = " !?.,MNBVCXYLKJHGFDSAPOIUZTREWQ";
+            int i = 0;
+            foreach (Control ctrl in panel1.Controls)
+            {
+                if (ctrl is Button)
+                    (ctrl as Button).Text = pismena[i].ToString();
+                i++;
+            }
+        }
+
+        private void OnClick_key(object sender, EventArgs e)
+        {
+            textBox1.Text += (sender as Button).Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                textBox2.Text = "";
+                string text = textBox1.Text;
+                foreach (char znak in text)
+                {
+                    int ascii = Convert.ToInt32(znak);
+                    if (ascii >= 88 && ascii <= 90)
+                        ascii -= 23;
+                    else if (znak != '?' && znak != '!' && znak != ',' && znak != '.' && znak != ' ')
+                    {
+                        ascii += 3;
+                    }
+                    textBox2.Text += Convert.ToChar(ascii);
+                }
+            }
         }
     }
 }
---
+++ příklad - 3/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace příklad___3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocet = 0;
+            double prumer = 0;
+            foreach (Control ctrl in groupBox1.Controls)
+            {
+                if (ctrl is TextBox && (ctrl as TextBox).Text != "")
+                {
+                    prumer += int.Parse((ctrl as TextBox).Text);
+                    pocet++;
+                }
+            }
+            if (pocet != 0)
+                prumer = prumer / pocet;
+            label7.Text = "Průměr: " + prumer;
+
+            string text = "";
+            foreach (string item in listBox1.SelectedItems)
+                text += item + ", ";
+
+            foreach (Control ctrl in groupBox1.Controls)
+                if (ctrl is Label)
+                    (ctrl as Label).Text = text;
+        }
+    }
+}
